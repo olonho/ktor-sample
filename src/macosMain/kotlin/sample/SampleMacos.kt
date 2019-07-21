dc9565e7ad6fa1ab8c0af90ff11e7ef1f52862b6
Nikolay Igotti <igotti@gmail.com>
2019-07-21 11:24:50 +0300

    Background fetcher.

--- src/macosMain/kotlin/sample/SampleMacos.kt
+++ src/macosMain/kotlin/sample/SampleMacos.kt
@@ -1,14 +1,18 @@
 package sample
 
 import io.ktor.client.*
+import io.ktor.client.engine.curl.Curl
 import io.ktor.client.request.*
 import kotlinx.coroutines.runBlocking
 import kotlinx.serialization.SerializationException
 import kotlinx.serialization.json.*
+import kotlin.native.SharedImmutable
+import kotlin.native.ThreadLocal
+import kotlin.native.concurrent.*
 
-fun main() = runBlocking {
+fun fetchSimple(url: String) = runBlocking {
     val client = HttpClient()
-    val result = client.get<String>("https://jsonplaceholder.typicode.com/todos/1")
+    val result = client.get<String>(url)
     println("Result: $result")
     val jsonParser = Json(JsonConfiguration.Stable)
     val json = try {
@@ -18,4 +22,32 @@ fun main() = runBlocking {
     }
     println("JSON : $json id = ${json.jsonObject["id"]}")
     client.close()
+}
+
+val backgroundWorker = Worker.start()
+@ThreadLocal
+val jsonParser = Json(JsonConfiguration.Stable)
+@SharedImmutable
+val mailbox = AtomicReference<JsonElement?>(null)
+
+fun fetchBackground(url: String) {
+    val future = backgroundWorker.execute(TransferMode.SAFE, { url }) {
+        val client = HttpClient(Curl)
+        val result = runBlocking {
+            client.get<String>(it)
+        }
+        mailbox.value = (try {
+            jsonParser.parseJson(result)
+        } catch (e: SerializationException) {
+            JsonPrimitive("<error: ${e.message}>")
+        }).freeze()
+        "OK"
+    }
+    println(future.result)
+    println(mailbox.value)
+}
+
+fun main() {
+    fetchSimple("https://jsonplaceholder.typicode.com/todos/1")
+    fetchBackground("https://jsonplaceholder.typicode.com/todos/2")
 }
